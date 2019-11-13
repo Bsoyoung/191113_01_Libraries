3a66a3697e8103c87503463bef948f8f0f07aff7
so_young <qksthdhd94@gmail.com>
2019-11-13 19:40:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a191113_01_libraries/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a191113_01_libraries
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    var mContext:Context = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
