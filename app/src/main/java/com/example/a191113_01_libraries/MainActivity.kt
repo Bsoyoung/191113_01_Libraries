41e80ea5dc45c0310a2294dd04992e415a86b41f
so_young <qksthdhd94@gmail.com>
2019-11-13 19:51:10 +0900

    Glide를 이용한 이미지 로딩 예제

--- app/src/main/java/com/example/a191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/example/a191113_01_libraries/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.a191113_01_libraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -17,6 +19,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        var url = "https://mblogthumb-phinf.pstatic.net/MjAxNzEwMTFfMTIg/MDAxNTA3Njg4OTE2Nzc0.bfpf13NNrJQ47nOad71EHrIGLwke2YRowmdw_nCdw1Yg.Ww7wKhtCxPmODHQv59ll6X99x6UBAGqf9rapYlMM82kg.PNG.sabins1112/2017-10-11_11%3B27%3B51.PNG?type=w2"
+        Glide.with(mContext).load(url).into(profileImg)
+
     }
 
 }
